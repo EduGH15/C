c484fb4c4d7cd4931c71da23b9cf35388746906e
EduGH15 <eduardogl15.personal@gmail.com>
2023-03-26 20:45:38 -0300

    Añadiendo el ejercicio 5

---
+++ ejercicio5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//Crear un programa que a partir de los depredaroes capturados por cada uno, imprima por pantalla "Te capturaremos 626!!!" si entre los dos lograron llegar a 10 o 
+//"Maldito hamster incompetente" en caso contrario.
+
+int main(){
+    int cazador1;   
+    int cazador2;
+    int total;
+
+    printf("Ingrese la cantidad de depredadores capturados por el cazador 1:\n");
+    scanf("%i", &cazador1);
+    printf("Ingrese la cantidad de depredadores capturados por el cazador 2:\n");
+    scanf("%i", &cazador2);
+
+    total = cazador1 + cazador2;
+
+    if(total >= 10){
+        printf("Te capturaremos 626!!!\n");
+    }else{
+        printf("Maldito hamster incopetente\n");
+    }
+    
+    return 0;
+}
