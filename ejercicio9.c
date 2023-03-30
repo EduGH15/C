564b19ff5285d5617bc795f292686ddf9f97bbbb
EduGH15 <eduardogl15.personal@gmail.com>
2023-03-30 16:31:08 -0300

    Agrego el ejercicio 9 implementando el uso de strings

---
+++ ejercicio9.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+/*Clayton, cansado de los métodos de Jane y el profesor Porter, decide emplear un método más arcaico para hacerle entender a Tarzán que quieren ver gorilas. El método se basa en decir la palabra “gorila” y hacer que Tarzán la repita hasta que este entendiera a qué se refiere, aumentando el tono con cada repetición (https://www.youtube.com/watch?v=533JK0BrGOQ). El profesor Porter, sorprendido, decide contar cuántas veces se repitió este diálogo entre los dos. Por eso se pide:
+
+    Imprimir por pantalla esta discusión, sabiendo la cantidad total que contó el profesor Porter.
+
+Ejemplos:
+
+Si se recibe 1, debe imprimirse:
+GO-RI-LA
+GO-RI-LA!
+
+Si se recibe 2, debe imprimirse:
+GO-RI-LA
+GO-RI-LA!
+GO-RI-LA!!
+GO-RI-LA!!!*/
+
+int main(){
+    char string[100];
+	int veces_repetidas;
+	scanf("%i", &veces_repetidas);
+
+    for(int i = 0; i < (veces_repetidas * 2); i++){
+        printf("GO-RI-LA%s\n", string);
+        strcat(string, "!");
+    }
+	// Aqui va tu codigo...
+
+	return 0;
+}
